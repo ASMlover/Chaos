63749cc742c0f1cce610ef3870855d60cc25f5a2
ASMlover <ASMlover@126.com>
2016-12-27 10:09:32 +0800

    :sparkles: feat(traits): add traits wrapper

---
+++ Chaos/Utility/Traits.h
@@ -0,0 +1,107 @@
+// Copyright (c) 2016 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#ifndef CHAOS_UTILITY_TRAITS_H
+#define CHAOS_UTILITY_TRAITS_H
+
+#include <type_traits>
+
+namespace Chaos {
+
+// disable_if `traits`
+template <bool B, typename T = void>
+struct DisableIfImpl {
+  typedef T type;
+};
+
+template <typename T>
+struct DisableIfImpl<true, T> {};
+
+template <typename Cond, typename T = void>
+struct DisableIf : public DisableIfImpl<Cond::value, T> {};
+
+// add reference `traits`
+template <typename T>
+struct AddRefImpl {
+  typedef T& type;
+};
+
+template <typename T>
+struct AddRefImpl<T&&> {
+  typedef T&& type;
+};
+
+template <typename T>
+struct AddRef {
+  typedef typename AddRefImpl<T>::type type;
+};
+
+template <typename T>
+struct AddRef<T&> {
+  typedef T& type;
+};
+
+template <typename Cond, typename T = void>
+using DisableIf_t = typename DisableIf<Cond, T>::type;
+
+template <typename T>
+using AddRef_t = typename AddRef<T>::type;
+
+#if __cplusplus < 201402L
+  template <typename T>
+  using RemoveCV_t = typename std::remove_cv<T>::type;
+
+  template <typename T>
+  using RemovePtr_t = typename std::remove_pointer<T>::type;
+
+  template <typename T>
+  using RemoveRef_t = typename std::remove_reference<T>::type;
+
+  template <bool B, typename T = void>
+  using EnableIf_t = typename std::enable_if<B, T>::type;
+
+  template <typename T>
+  using AddPtr_t = typename std::add_pointer<T>::type;
+#else
+  template <typename T>
+  using RemoveCV_t = std::remove_cv_t<T>;
+
+  template <typename T>
+  using RemovePtr_t = std::remove_pointer_t<T>;
+
+  template <typename T>
+  using RemoveRef_t = std::remove_reference_t<T>;
+
+  template <bool B, typename T = void>
+  using EnableIf_t = typedef std::enable_if_t<B, T>;
+
+  template <typename T>
+  using AddPtr_t = std::add_pointer_t<T>;
+#endif
+
+}
+
+#endif // CHAOS_UTILITY_TRAITS_H

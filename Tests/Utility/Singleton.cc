8de3eb81baa5228793713de2bd09f44dd1ce8c2f
ASMlover <asmlover@126.com>
2017-03-11 21:11:18 +0800

    :white_check_mark: test(singleton): add unittest for singleton utility tools

---
+++ Tests/Utility/Singleton.cc
@@ -0,0 +1,68 @@
+// Copyright (c) 2017 ASMlover. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list ofconditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materialsprovided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+#include <string>
+#include <thread>
+#include <Chaos/UnCopyable.h>
+#include <Chaos/Concurrent/CurrentThread.h>
+#include <Chaos/Logging/Logging.h>
+#include <Chaos/Unittest/TestHarness.h>
+#include <Chaos/Utility/Singleton.h>
+
+class _Useless : private Chaos::UnCopyable {
+  std::string message_;
+public:
+  _Useless(void) {
+    CHAOSLOG_INFO << "Chaos::Singleton unittest  - _Useless::_Useless @tid=" << Chaos::CurrentThread::get_tid();
+  }
+
+  ~_Useless(void) {
+    CHAOSLOG_INFO << "Chaos::Singleton unittest - _Useless::~_Useless @tid="
+      << Chaos::CurrentThread::get_tid() << ", @message=" << message_;
+  }
+
+  void set_message(const std::string& message) {
+    message_ = message;
+  }
+
+  const std::string& get_message(void) const {
+    return message_;
+  }
+};
+
+CHAOS_TEST(Singleton, Chaos::FakeTester) {
+  {
+    Chaos::Singleton<_Useless>::get_instance().set_message("_Useless.Main");
+    std::thread t([] {
+        CHAOSLOG_INFO << "Chaos::Singleton unittest - @tid="
+          << Chaos::CurrentThread::get_tid() << ", @message=" << Chaos::Singleton<_Useless>::get_instance().get_message();
+        Chaos::Singleton<_Useless>::get_instance().set_message("_Useless.Changed");
+        });
+    t.join();
+    CHAOSLOG_INFO << "Chaos::Singleton unittest - @tid="
+      << Chaos::CurrentThread::get_tid() << ", @message=" << Chaos::Singleton<_Useless>::get_instance().get_message();
+  }
+}

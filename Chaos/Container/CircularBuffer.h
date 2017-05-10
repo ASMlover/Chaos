49ea8a5150fb847a00247b397c1e1b1ce2a35b2f
ASMlover <asmlover@126.com>
2017-05-11 02:30:30 +0800

    :construction: chore(circbuf): updated the internal type definitions for circular buffer

--- Chaos/Container/CircularBuffer.h
+++ Chaos/Container/CircularBuffer.h
@@ -151,32 +151,20 @@ public:
 template <typename T, typename Allocator = std::allocator<T>>
 class CircularBuffer {
 public:
-  typedef CircularBuffer<T, Allocator>
-    SelfType;
-  typedef Allocator
-    allocator_type;
-  typedef typename allocator_type::value_type
-    value_type;
-  typedef typename allocator_type::size_type
-    size_type;
-  typedef typename allocator_type::pointer
-    pointer;
-  typedef typename allocator_type::const_pointer
-    const_pointer;
-  typedef typename allocator_type::reference
-    reference;
-  typedef typename allocator_type::const_reference
-    const_reference;
-  typedef typename allocator_type::difference_type
-    difference_type;
-  typedef CircularBufferIterator<SelfType>
-    iterator;
-  typedef CircularBufferIterator<const SelfType, const value_type>
-    const_iterator;
-  typedef std::reverse_iterator<iterator>
-    reverse_iterator;
-  typedef std::reverse_iterator<const_iterator>
-    const_reverse_iterator;
+  typedef CircularBuffer<T, Allocator>             SelfType;
+  typedef Allocator                                allocator_type;
+  typedef typename allocator_type::value_type      value_type;
+  typedef typename allocator_type::size_type       size_type;
+  typedef typename allocator_type::pointer         pointer;
+  typedef typename allocator_type::const_pointer   const_pointer;
+  typedef typename allocator_type::reference       reference;
+  typedef typename allocator_type::const_reference const_reference;
+  typedef typename allocator_type::difference_type difference_type;
+  typedef CircularBufferIterator<SelfType>         iterator;
+  typedef CircularBufferIterator
+    <const SelfType, const value_type>             const_iterator;
+  typedef std::reverse_iterator<iterator>          reverse_iterator;
+  typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
 private:
   pointer buff_{};
   size_type capacity_{};

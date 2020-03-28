e5945f72ddf950d1e7ce493b2420728dd166acb0
ASMlover <asmlover@126.com>
2020-03-28 21:59:44 +0800

    :construction: chore(types): updated the declaration of types util functions

--- Chaos/Base/Types.h
+++ Chaos/Base/Types.h
@@ -66,22 +66,22 @@ using byte_t = std::uint8_t;
 namespace Chaos {
 
 template <typename T>
-inline const T& chaos_min(const T& a, const T& b) {
+inline const T& chaos_min(const T& a, const T& b) noexcept {
   return a < b ? a : b;
 }
 
 template <typename T>
-inline const T& chaos_max(const T& a, const T& b) {
+inline const T& chaos_max(const T& a, const T& b) noexcept {
   return a > b ? a : b;
 }
 
 template <typename T>
-inline T* get_pointer(const std::unique_ptr<T>& p) {
+inline T* get_pointer(const std::unique_ptr<T>& p) noexcept {
   return p.get();
 }
 
 template <typename T>
-inline T* get_pointer(const std::shared_ptr<T>& p) {
+inline T* get_pointer(const std::shared_ptr<T>& p) noexcept {
   return p.get();
 }
 
@@ -90,22 +90,23 @@ template <typename T> struct Identity {
 };
 
 template <typename T>
-inline T implicit_cast(typename Identity<T>::Type x) {
+inline T implicit_cast(typename Identity<T>::Type x) noexcept {
   return x;
 }
 
 template <typename Target, typename Source>
-inline Target down_cast(Source& x) {
+inline Target down_cast(Source& x) noexcept {
   return static_cast<Target>(x);
 }
 
 template <typename Target, typename Source>
-inline Target down_cast(Source* x) {
+inline Target down_cast(Source* x) noexcept {
   return static_cast<Target>(x);
 }
 
 template <typename Target, typename Source>
-inline std::shared_ptr<Target> down_cast_ptr(const std::shared_ptr<Source>& p) {
+inline std::shared_ptr<Target>
+down_cast_ptr(const std::shared_ptr<Source>& p) noexcept {
   return std::static_pointer_cast<Target>(p);
 }
 

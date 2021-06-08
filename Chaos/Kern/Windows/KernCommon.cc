a29c1e755bc8215a2e7546094af8ff35be497f6e
ASMlover <asmlover@126.com>
2021-06-08 23:29:54 +0800

    :bug: fixed(kern): fixed import bug

--- Chaos/Kern/Windows/KernCommon.cc
+++ Chaos/Kern/Windows/KernCommon.cc
@@ -1,5 +1,13 @@
 // Copyright (c) 2016 ASMlover. All rights reserved.
 //
+//  ____     __
+// /\  _`\  /\ \
+// \ \ \/\_\\ \ \___      __      ___     ____
+//  \ \ \/_/_\ \  _ `\  /'__`\   / __`\  /',__\
+//   \ \ \L\ \\ \ \ \ \/\ \L\.\_/\ \L\ \/\__, `\
+//    \ \____/ \ \_\ \_\ \__/.\_\ \____/\/\____/
+//     \/___/   \/_/\/_/\/__/\/_/\/___/  \/___/
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
@@ -27,7 +35,7 @@
 #include <Windows.h>
 #include <DbgHelp.h>
 #include <process.h>
-#include <Chaos/Utility/Memory.h>
+#include <Chaos/Utility/Memory.hh>
 #include <Chaos/Kern/Windows/KernCommon.h>
 
 namespace Chaos {
